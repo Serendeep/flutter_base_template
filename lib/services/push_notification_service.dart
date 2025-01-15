2797da46ab335f1036ea6cb30d6f5d3edeed5363
CursedApple <serendeep4@gmail.com>
2025-01-15 16:59:51 +0530

    Fixed import issues

--- lib/services/push_notification_service.dart
+++ lib/services/push_notification_service.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_base_template/utils/config/app_config.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_base_template/utils/config/bloc_dispatcher.dart';
-import 'package:flutter_base_template/utils/config/app_config.dart';
 import 'package:logger/logger.dart';
 import 'package:rxdart/rxdart.dart';
 
